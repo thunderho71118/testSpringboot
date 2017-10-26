a0f599d6723edbf99d0420905063e207dc6e7421
Vend_sap00011 <Vend_sap00011@mtkapdev01.mediatek.inc>
2017-10-26 09:26:45 +0800

    test only commit

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -14,6 +14,7 @@ public class Application
 
 	public static void main(String[] args) throws Exception
 	{
+		System.out.println("maybe run!!!");
 		SpringApplication.run(Application.class, args);
 	}
 
